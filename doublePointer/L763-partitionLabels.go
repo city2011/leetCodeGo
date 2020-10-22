514b3bb9978103aa70106c9a66a5fa348757e512
huangcheng <huangcheng_1993@126.com>
2020-10-23 04:11:24 +0800

    go version leetcode

---
+++ doublePointer/L763-partitionLabels.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main () {
+	par := partitionLabels("abdkldddfgadgiojhwenw")
+	fmt.Print(par)
+}
+
+func partitionLabels(S string) (partition []int) {
+	lastPos := [26] int {}
+
+	for i := 0; i < len(S); i++ {
+		pos := S[i] - 'a'
+		lastPos[pos] = i
+	}
+
+	start, end := 0, 0
+	for i, c := range S {
+		if lastPos[c - 'a'] > end {
+			end = lastPos[c - 'a']
+		}
+		if i == end {
+			partition = append(partition, end - start + 1)
+			start = end + 1
+		}
+	}
+	return
+}
