223539d3056eaccac8a428805f838091081ca02b
huangcheng04 <huangcheng04@meituan.com>
2020-10-23 16:10:32 +0800

    2020/10/23 同步第844题

---
+++ doublePointer/L844.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	ans := backspaceCompare("a##c", "#a#c")
+	fmt.Print(ans)
+	fmt.Println("hello, go")
+}
+
+func backspaceCompare(S string, T string) bool {
+	skipS, skipT := 0, 0
+	i, j := len(S) - 1, len(T) - 1
+	for i >= 0 {
+		for i >= 0 {
+			if S[i] == '#' {
+				skipS++
+				i--
+			} else if skipS > 0{
+				skipS--
+				i--
+			} else {
+				break
+			}
+		}
+
+		for j >= 0 {
+			if T[j] == '#' {
+				skipT++
+				j--
+			} else if skipT > 0 {
+				skipT--
+				j--
+			} else {
+				break
+			}
+		}
+
+		if i >= 0 && j >= 0 {
+			if S[i] != T[j] {
+				return false
+			}
+		} else {
+			if i >= 0 || j >= 0 {
+				return false
+			}
+		}
+		i--
+		j--
+	}
+	return false
+}
