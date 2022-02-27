3fe66b1bef0cdea152b0ae7a889927b5611089be
city2011 <huangcheng_1993@126.com>
2022-02-27 23:26:53 +0800

    daily exercies

---
+++ L553.go
@@ -0,0 +1,23 @@
+package main
+
+import "strconv"
+
+func main() {
+
+}
+
+func optimalDivision(nums []int) string {
+	if len(nums) == 1 {
+		return strconv.Itoa(nums[0])
+	}
+	if len(nums) == 2 {
+		return strconv.Itoa(nums[0]) + "/" + strconv.Itoa(nums[1])
+	}
+	var res string
+	res = strconv.Itoa(nums[0]) + "/("
+	for i := 1; i < len(nums) - 1; i++ {
+		res += strconv.Itoa(nums[i]) + "/"
+	}
+	res += strconv.Itoa(nums[len(nums)-1]) + ")"
+	return res
+}
--- jianzhi/J6.go
+++ jianzhi/J6.go
@@ -1,7 +1,10 @@
 package main
 
 func main() {
-
+	t := ListNode{2, nil}
+	a1 := ListNode{1, &t}
+	a2 := ListNode{3, &a1}
+	reversePrint2(&a2)
 }
 
 /**
@@ -25,4 +28,22 @@ func reversePrint(head *ListNode) []int {
 		si[j] = tmp
 	}
 	return si
+}
+
+func reversePrint2(head *ListNode) []int {
+	var res []int
+	var newHead *ListNode
+	cur := head
+	for ;cur != nil; {
+		tmp := cur.Next
+		cur.Next = newHead
+		newHead = cur
+		cur = tmp
+	}
+
+	for ;newHead != nil; {
+		res = append(res, newHead.Val)
+		newHead = newHead.Next
+	}
+	return res
 }
