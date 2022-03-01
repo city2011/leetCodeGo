889c548dd30e956c64d11c0a0496a5e35e50adab
city2011 <huangcheng_1993@126.com>
2022-03-02 01:54:20 +0800

    simple exercise

---
+++ jianzhi/J05.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(replaceSpace2("We are happy."));
+}
+
+func replaceSpace(s string) string {
+	var res string
+	for _,byte1 := range s {
+		if byte1 == ' ' {
+			res += "%20"
+		} else {
+			res += string(byte1)
+		}
+	}
+	return res
+}
+
+func replaceSpace2(s string) string {
+	b := []byte(s)
+	length := len(b)
+	spaceCount := 0
+	// 计算空格数量
+	for _, v := range b {
+		if v == ' ' {
+			spaceCount++
+		}
+	}
+	// 扩展原有切片
+	resizeCount := spaceCount * 2
+	tmp := make([]byte, resizeCount)
+	b = append(b, tmp...)
+	i := length - 1
+	j := len(b) - 1
+	for i < j {
+		if b[i] != ' ' {
+			b[j] = b[i]
+			i--
+			j--
+		} else {
+			b[j] = '0'
+			b[j-1] = '2'
+			b[j-2] = '%'
+			i--
+			j = j - 3
+		}
+	}
+	return string(b)
+}
---
+++ jianzhi/J58.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(reverseLeftWords3("abcdefg", 2))
+}
+
+func reverseLeftWords(s string, n int) string {
+	return s[n-1:len(s)] + s[0:n]
+}
+
+func reverseLeftWords2(s string, n int) string {
+	bs := make([]byte, len(s))
+	idx := 0
+	for i := n; i < len(s); i++ {
+		bs[idx] = s[i]
+		idx++
+	}
+	for i := 0; i < n; i++ {
+		bs[idx] = s[i]
+		idx++
+	}
+	return string(bs)
+}
+
+func reverseLeftWords3(s string, n int) string {
+	bs := make([]byte, len(s))
+	for i := n; i < len(s) + n; i++ {
+		bs[i - n] = s[i % len(s)]
+	}
+	return string(bs)
+}
