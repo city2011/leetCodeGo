00f1a9f74c9b543fa95397b073b450d7d7ba284a
city2011 <huangcheng_1993@126.com>
2022-03-17 23:36:39 +0800

    exercise2022

---
+++ L393.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	res := validUtf8([]int {250,145,145,145,145})
+	fmt.Println(res)
+}
+
+func validUtf8(data []int) bool {
+	m1 := 1 << 7
+	m2 := 1 << 6
+
+	n := len(data)
+
+	for idx := 0; idx < len(data); idx++{
+		nByte := 0
+		mask := 1 << 7
+		for mask & data[idx] > 0 {
+			nByte += 1
+			mask = mask >> 1
+		}
+		if nByte == 0 {
+			continue
+		}
+		nByte--
+		if nByte > n - idx - 1 || nByte == 0{
+			return false
+		}
+		for i := 0; i < nByte; i++ {
+			idx++
+			if ((data[idx] & m1) == 0) || ((data[idx] & m2) != 0) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
